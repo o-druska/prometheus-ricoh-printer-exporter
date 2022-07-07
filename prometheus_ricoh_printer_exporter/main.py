5128d597406507392989bdb201e0c872aa0df881
odruska <oskar.druska@alumni.fh-aachen.de>
2022-07-07 09:31:39 +0200

    removed debug print(args) statement minor code formatting

--- prometheus_ricoh_printer_exporter/main.py
+++ prometheus_ricoh_printer_exporter/main.py
@@ -12,6 +12,7 @@ from bs4 import BeautifulSoup as bs
 from prometheus_client import start_http_server, REGISTRY
 from . import exporter_file
 
+
 def main():
     url_oak = "http://oak.inm7.de/web/guest/en/websys/webArch/getStatus.cgi"
     url_pine = "http://pine.inm7.de/web/guest/en/websys/webArch/getStatus.cgi"
@@ -21,7 +22,6 @@ def main():
     urls = [url_oak, url_pine, url_willow, url_larch]
 
     args = get_parsed_args()
-    print("parsed args: " + str(args))
 
     try:
         soups = get_soups(urls, args.insecure)
@@ -30,9 +30,9 @@ def main():
         sys.exit(c.strerror)
     except TimeoutError as t:
         sys.exit(t.strerror)
-    
+
     REGISTRY.register(exporter_file.RicohPrinterExporter(soups=soups, urls=urls))
-    
+
     if args.listenaddress is None:
         start_http_server(port=9840, addr='127.0.0.1')
     else:
@@ -47,7 +47,8 @@ def main():
     while True:
         time.sleep(1)
 
-def get_soups(urls : list, insec_bool : bool):
+
+def get_soups(urls: list, insec_bool: bool):
     '''
     Returns a list containing a soup obejct for each printer in urls.
     '''
@@ -56,7 +57,7 @@ def get_soups(urls : list, insec_bool : bool):
     for url in urls:
         print("Connecting to: " + url + " ...")
 
-        r_url = requests.get(url, verify = not insec_bool)
+        r_url = requests.get(url, verify=not insec_bool)
         if r_url.status_code != 200:
             raise ConnectionError("Something's wrong with the Website:\n" + r_url + "\n" + str(r_url.status_code))
         soups.append(bs(r_url.text, 'html.parser'))
@@ -75,8 +76,8 @@ def get_parsed_args():
         type=str,
         dest='listenaddress',
         help='Address and port to expose metrics and web interface. Default: ":9840"\n'
-                'To listen on all interfaces, omit the IP. ":<port>"`\n'\
-                'To listen on a specific IP: <address>:<port>')
+        'To listen on all interfaces, omit the IP. ":<port>"`\n'
+        'To listen on a specific IP: <address>:<port>')
     mutual_group.add_argument(
         '-i', '--insecure',
         dest='insecure',
